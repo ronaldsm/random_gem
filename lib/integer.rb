1caadd8144df30cb5c609222283edbfe2814ff54
ronaldsm <ronaldsm@cisco.com>
2018-04-27 14:16:35 -0600

    rubocop recommended updates

--- lib/integer.rb
+++ lib/integer.rb
@@ -1,8 +1,9 @@
 module RandomGem
+  # Get random integers
   class Integer
     def self.get(min, max)
       spread = max - min
-      return max if spread == 0
+      return max if spread.zero?
       raise ArgumentError, 'Max must be greater than Min' if spread < 0
       Random.rand(spread) + min
     end
