48b7d91b2e7feb890e699e0e9be3358b23bfeb31
ronaldsm <ronaldsm@cisco.com>
2018-04-30 10:39:42 -0600

    Add charsets to string

--- lib/string.rb
+++ lib/string.rb
@@ -6,13 +6,16 @@ module RandomGem
     UPPER_ALPHA = [*'A'..'Z'].freeze
     HEX = %w[0 1 2 3 4 5 6 7 8 9 a b c d e f].freeze
 
-    def initialize(opts)
 
+    def initialize
+      @charset = []
+      @charset += INTEGERS
+      @charset += LOWER_ALPHA
+      @charset += UPPER_ALPHA
     end
 
-    def self.get(length)
-      (0...length).map { LOWER_ALPHA.sample }.join
-      # (0...size).map{ charset.to_a[rand(charset.size)] }.join
+    def get(length)
+      (0...length).map { @charset.sample }.join
     end
   end
 end
--- spec/random_gem_string_spec.rb
+++ spec/random_gem_string_spec.rb
@@ -2,11 +2,11 @@ require 'pry-byebug'
 
 RSpec.describe RandomGem::String do
   it 'returns a string' do
-    expect(RandomGem::String.get(1)).to be_a(String)
+    expect(RandomGem::String.new.get(1)).to be_a(String)
   end
 
   it 'returns a string of the appropriate length' do
-    str = RandomGem::String.get(10)
+    str = RandomGem::String.new.get(10)
     puts str
     expect(str).to be_a(String)
     expect(str.length).to eql 10
